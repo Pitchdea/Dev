11814ef9d279f18640a80f0a751b0eee9b3fc17f
Tero <tero.urponen@gmail.com>
2014-02-18 13:18:16 +0200

    Removed Watin, implemented selenium for IE 10/11 support.

---
+++ PitchdeaProduct/Pitchdea.Specs.Selenium/LoginFunctionSteps.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.IE;
+using TechTalk.SpecFlow;
+
+namespace Pitchdea.Specs.Selenium
+{
+    [Binding]
+    public class LoginFunctionSteps
+    {
+        public const string BaseUrl = "http://localhost:28231/";
+
+        [Given(@"page ""(.*)"" is open")]
+        public void GivenPageIsOpen(string url)
+        {
+            IWebDriver driver = new InternetExplorerDriver(@"..\..\Tools\IEDriver");
+            var root = new Uri(BaseUrl);
+            var absoluteUrl = new Uri(root, url);
+            driver.Navigate().GoToUrl(absoluteUrl);
+            Assert.AreEqual(absoluteUrl, driver.Url);
+        }
+        
+        [Then(@"page title should be ""(.*)""")]
+        public void ThenPageTitleShouldBe(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
--- PitchdeaProduct/Pitchdea.Specs/FirefoxPortableLegacy20/Other/Source/PortableApps.comInstallerCustom.nsh
+++
@@ -1,4 +0,0 @@
-!macro CustomCodePreInstall
-	CreateDirectory "$INSTDIR\Data\settings"
-	WriteINIStr "$INSTDIR\Data\settings\FirefoxPortableSettings.ini" "FirefoxPortableSettings" "AgreedToLicense" "2"
-!macroend
