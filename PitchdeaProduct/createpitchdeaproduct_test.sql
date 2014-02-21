cefc0042bd60728e64564bf6b2f8aca0c836166d
tapio-louhi <tapio.louhi@gmail.com>
2014-02-21 17:04:35 +0200

    fixd schema create typo

--- PitchdeaProduct/createpitchdeaproduct_test.sql
+++ PitchdeaProduct/createpitchdeaproduct_test.sql
@@ -31,7 +31,7 @@ CREATE TABLE `user` (
   `password` char(44) DEFAULT NULL,
   PRIMARY KEY (`userid`)
 )
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
