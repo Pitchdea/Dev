60a3ce4d694122422c3748a3da913341792879c1
Tero <tero.urponen@gmail.com>
2014-03-03 19:46:45 +0200

    LoginStatusControl.feature and steps done

--- PitchdeaProduct/Pitchdea.Specs.Selenium/Steps/LoginStatusControlSteps.cs
+++ PitchdeaProduct/Pitchdea.Specs.Selenium/Steps/LoginStatusControlSteps.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Threading;
+using NUnit.Framework;
+using OpenQA.Selenium;
 using Pitchdea.Core;
 using Pitchdea.Core.Test.Utils;
+using Pitchdea.Specs.Selenium.Utils;
 using TechTalk.SpecFlow;
 
 namespace Pitchdea.Specs.Selenium.Steps
@@ -11,8 +15,41 @@ namespace Pitchdea.Specs.Selenium.Steps
         [Given(@"user is logged in as ""(.*)""")]
         public void GivenUserIsLoggedInAs(string username)
         {
-            var authenticator = new Authenticator(SqlTestTool.TestConnectionString);
-            throw new NotImplementedException();
+            const string email = "test@pitchdea.com";
+            const string password = "password123";
+
+            var auth = new Authenticator(SqlTestTool.TestConnectionString);
+
+            auth.RegisterNewUser(username, email, password);
+
+            var root = new Uri(WebBrowser.BaseUrl);
+            var absoluteUrl = new Uri(root, "/loginPage.aspx");
+            WebBrowser.Current.Navigate().GoToUrl(absoluteUrl);
+            Assert.AreEqual(absoluteUrl, WebBrowser.Current.Url);
+
+            IWebElement emailBox = WebBrowser.Current.FindElement(By.Id("MainContent_emailTextBox"));
+            emailBox.SendKeys(email);
+
+            IWebElement passwordBox = WebBrowser.Current.FindElement(By.Id("MainContent_passwordTextBox"));
+            passwordBox.SendKeys(password);
+
+            IWebElement fieldElement = WebBrowser.Current.FindElement(By.Id("MainContent_loginButton"));
+            fieldElement.Click();
+            Thread.Sleep(1000);
         }
+
+        [Then(@"""(.*)"" link should be on the page")]
+        public void ThenLinkShouldBeOnThePage(string linkText)
+        {
+            IWebElement linkElement = WebBrowser.Current.FindElement(By.LinkText(linkText));
+            Assert.NotNull(linkElement);
+        }
+
+        [Then(@"""(.*)"" link should not be on the page")]
+        public void ThenLinkShouldNotBeOnThePage(string linkText)
+        {
+            Assert.Throws<NoSuchElementException >(() => WebBrowser.Current.FindElement(By.LinkText(linkText)));
+        }
+
     }
 }
