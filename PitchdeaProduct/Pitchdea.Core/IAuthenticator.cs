614e323aae0d81961e5b110923975710d243f658
tapio-louhi <tapio.louhi@gmail.com>
2014-03-04 10:23:51 +0200

    updated IAuthenticator summaries

--- PitchdeaProduct/Pitchdea.Core/IAuthenticator.cs
+++ PitchdeaProduct/Pitchdea.Core/IAuthenticator.cs
@@ -12,7 +12,7 @@ namespace Pitchdea.Core
     public interface IAuthenticator
     {
         /// <summary>
-        /// Registers a new user, checks that username or email does not exist. Inserts all required information to the database. 
+        /// Registers a new user, Inserts all required information to the database. 
         /// </summary>
         /// <param name="user">Username to register</param>
         /// <param name="email">Email to register</param>
@@ -20,10 +20,18 @@ namespace Pitchdea.Core
         /// <returns>User info for the registered user or null if registration wasn't successful.</returns>
         UserInfo RegisterNewUser(string user, string email, string password);
 
-        //Checks if the inputted username already exists in the database already. <returns> error message if exists</returns>
+        /// <summary>
+        /// Checks if the username already exists in the database
+        /// </summary>
+        /// <param name="user"></param>
+        /// <returns>True if the username exists, else null.</returns>
         bool CheckIfUsernameExists(string user);
-        
-        //Checks if the inputted email already exists in the database already. <returns> error message if exists</returns>
+
+        /// <summary>
+        /// Checks if the email already exists in the database
+        /// </summary>
+        /// <param name="email"></param>
+        /// <returns>True if the email address exists, else null.</returns>
         bool CheckIfEmailExists(string email);
     }
     public class UserInfo
