45f5e7e6264c1c3cdc90fd149743880f70c2ac05
Tero <tero.urponen@gmail.com>
2014-03-05 01:10:29 +0200

    inverted ifs

--- PitchdeaProduct/Pitchdea/createIdeaPage.aspx.cs
+++ PitchdeaProduct/Pitchdea/createIdeaPage.aspx.cs
@@ -39,43 +39,40 @@ namespace Pitchdea
 
         protected void uploadImageButton_OnClick(object sender, EventArgs e)
         {
-        // server location where the images are stored.
-        string savePath = @"\images\ideapics\";
+            // server location where the images are stored.
+            string savePath = @"\images\ideapics\";
 
-        // Verify that the ImgUpload controller has the file.
-        if (ImgUpload.HasFile)
-        {                
-            // Allow only smaller than 100kB files.
-            if (ImgUpload.PostedFile.ContentLength < 102400)
+            // Verify that the ImgUpload controller has the file.
+            if (!ImgUpload.HasFile)
             {
-                // Allow only .jpg files to be uploaded. TODO: This is still not safe, attacker could circumwent by renaming their malicious file.
-                if (Path.GetExtension(ImgUpload.FileName) == ".jpg")
-                {
-                    // Append the name of the uploaded file to the path.
-                    savePath += Server.HtmlEncode(ImgUpload.FileName);
-                    //TODO: Resize the file, 300px*300px perhaps?
-                    // Saves the image to the specified path. If a file with the same name already exists it will be overwritten.  
-                    ImgUpload.SaveAs(savePath);
-
-                    // Notify the user their file was uploaded successfully.
-                    uploadStatusLabel.Text = "Your image was uploaded successfully.";
-                }
-                else 
-                    // Notify the user their file had wrong extension
-                    uploadStatusLabel.Text = "Oops! Your file was not a .jpg image.";
+                // Notify the user their file was not uploaded.
+                uploadStatusLabel.Text = "Oops! You forgot to specify an image to upload.";
+                return;
             }
-            else
+
+            // Allow only smaller than 100kB files.
+            if (ImgUpload.PostedFile.ContentLength >= 102400)
             {
                 // Notify the user their file was too big.
                 uploadStatusLabel.Text = "Oops! Your image was bigger than the maximum 100kB.";
+                return;
             }
-        }   
-        else
-        {
-            // Notify the user their file was not uploaded.
-            uploadStatusLabel.Text = "Oops! You forgot to specify an image to upload.";
-        }
 
+            // Allow only .jpg files to be uploaded. TODO: This is still not safe, attacker could circumwent by renaming their malicious file.
+            if (Path.GetExtension(ImgUpload.FileName) != ".jpg")
+            {
+                uploadStatusLabel.Text = "Oops! Your file was not a .jpg image.";
+                return;
+            }
+
+            // Append the name of the uploaded file to the path.
+            savePath += Server.HtmlEncode(ImgUpload.FileName);
+            //TODO: Resize the file, 300px*300px perhaps?
+            // Saves the image to the specified path. If a file with the same name already exists it will be overwritten.  
+            ImgUpload.SaveAs(savePath);
+
+            // Notify the user their file was uploaded successfully.
+            uploadStatusLabel.Text = "Your image was uploaded successfully.";
         }
     }
 }
