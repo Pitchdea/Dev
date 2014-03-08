0090c7eaf386b8ae49fe9e6f93e298e8979b57be
Tero <tero.urponen@gmail.com>
2014-03-09 01:09:37 +0200

    a few fixes

--- PitchdeaProduct/createpitchdeaproduct.sql
+++ PitchdeaProduct/createpitchdeaproduct.sql
@@ -40,23 +40,6 @@ CREATE TABLE `idea` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `test`
---
-
-DROP TABLE IF EXISTS `test`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `test` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `integerNumber` int(11) DEFAULT NULL,
-  `textString` varchar(45) DEFAULT NULL,
-  `floatNumber` float DEFAULT NULL,
-  `doubleNumber` double DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `user`
 --
 
