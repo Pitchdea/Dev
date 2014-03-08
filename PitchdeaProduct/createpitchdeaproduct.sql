e9fc11c7f19b215c9513ee7062382d7ca1a4d316
Tero <tero.urponen@gmail.com>
2014-03-08 23:59:31 +0200

    updated product sql and web.config to support required stuff on the server

--- PitchdeaProduct/createpitchdeaproduct.sql
+++ PitchdeaProduct/createpitchdeaproduct.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE  IF NOT EXISTS `pitchdeaproduct` /*!40100 DEFAULT CHARACTER SET utf8 */;
 USE `pitchdeaproduct`;
--- MySQL dump 10.13  Distrib 5.6.13, for Win32 (x86)
+-- MySQL dump 10.13  Distrib 5.6.11, for Win32 (x86)
 --
 -- Host: localhost    Database: pitchdeaproduct
 -- ------------------------------------------------------
--- Server version	5.6.15
+-- Server version	5.6.13-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,6 +18,45 @@ USE `pitchdeaproduct`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `idea`
+--
+
+DROP TABLE IF EXISTS `idea`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `idea` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hash` varchar(44) DEFAULT NULL,
+  `title` varchar(70) NOT NULL,
+  `summary` varchar(200) NOT NULL,
+  `description` text NOT NULL,
+  `question` varchar(90) NOT NULL,
+  `imagePath` varchar(45) DEFAULT NULL,
+  `userId` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `Id_idx` (`userId`),
+  CONSTRAINT `Id` FOREIGN KEY (`userId`) REFERENCES `user` (`userid`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `test`
+--
+
+DROP TABLE IF EXISTS `test`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `integerNumber` int(11) DEFAULT NULL,
+  `textString` varchar(45) DEFAULT NULL,
+  `floatNumber` float DEFAULT NULL,
+  `doubleNumber` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `user`
 --
 
@@ -26,12 +65,12 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `userid` int(11) NOT NULL AUTO_INCREMENT,
-  `email` varchar(100) DEFAULT NULL,
-  `salt` char(44) DEFAULT NULL,
-  `password` char(44) DEFAULT NULL,
+  `username` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `salt` char(44) NOT NULL,
+  `password` char(44) NOT NULL,
   PRIMARY KEY (`userid`)
-)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -43,4 +82,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-02-20 11:30:31
+-- Dump completed on 2014-03-06 14:57:54
