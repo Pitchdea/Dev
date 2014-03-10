a284858d6827e9961b6b700e41ea00d9ac9333c0
Tero <tero.urponen@gmail.com>
2014-03-10 09:20:19 +0200

    default page for redirecting

---
+++ PitchdeaProduct/Pitchdea/default.aspx.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.UI;
+
+namespace Pitchdea
+{
+    public partial class Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Redirect("~/mainPage.aspx");
+        }
+    }
+}
